698859233264c2b08086388d5af98f97913d365b
ShinJaehun <gunsmoke.shin@gmail.com>
2023-09-25 21:59:34 +0900

    cupcake-app : 프래그먼트 간 공유되는 ViewModel

---
+++ android-basics-kotlin-cupcake-app/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,96 @@
+package com.example.cupcake.model
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val TAG = "OrderViewModel"
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
+class OrderViewModel : ViewModel() {
+
+//    companion object {
+//    }
+
+    private val _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> = _flavor
+
+    val dateOptions: List<String> = getPickupOptions()
+
+    private val _date = MutableLiveData<String>()
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<String> = Transformations.map(_price) {
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
+    init {
+        resetOrder()
+    }
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+        updatePrice()
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun hasNoFlavorSet() : Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+        updatePrice()
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+
+        val formatter = SimpleDateFormat("MMM d E", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+
+//        options.forEach {option ->
+//            Log.i(TAG, "options: ${option}")
+//        }
+
+        return options
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    private fun updatePrice() {
+//        _price.value = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+            Log.i(TAG, "calculatedPrice : ${calculatedPrice.toString()}")
+            Log.i(TAG, "dateOptions[0] : ${dateOptions[0].toString()}")
+            Log.i(TAG, "_date.value : ${_date.value.toString()}")
+        }
+        _price.value = calculatedPrice
+    }
+
+}
